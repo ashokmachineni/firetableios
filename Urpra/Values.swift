7fe97116e01e5737d9139e9c785b3bc900f958be
ashokmachineni <ashok5679m@gmail.com>
2016-11-09 19:16:04 -0500

    fire-tablevie-with-title

---
+++ Urpra/Values.swift
@@ -0,0 +1,13 @@
+//
+//  Values.swift
+//  Urpra
+//
+//  Created by Ashok Machineni on 11/9/16.
+//  Copyright © 2016 Ashok Machineni. All rights reserved.
+//
+
+import Foundation
+class Values: NSObject {
+    var title: String?
+}
+
--- Urpra/ViewController.swift
+++ Urpra/ViewController.swift
@@ -8,18 +8,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import Firebase
 
+
+class ViewController: UITableViewController {
+    
+    var ref: FIRDatabaseReference!
+    var refHandle: UInt!
+    var valueList = [Values]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        ref = FIRDatabase.database().reference()
+        
+        fetchFirebaseData()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func fetchFirebaseData() {
+        
+        
+        refHandle = ref.child("SwiftJson").observe(.childAdded, with:
+            {(snapshot) in
+                if let dictionary = snapshot.value as? [String : AnyObject] {
+                    print(dictionary)
+                    
+                    let value = Values()
+                    
+                    value.setValuesForKeys(dictionary)
+                    self.valueList.append(value)
+                    
+                    
+                    self.tableView.reloadData()
+                    
+                    
+                }
+                
+        })
+        
+        
     }
-
-
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return valueList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let value = valueList[indexPath.row]
+        
+        cell.textLabel?.text = value.title
+        
+        return cell
+        
+    }
 }
-
